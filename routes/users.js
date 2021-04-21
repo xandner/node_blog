e1f5ffd795df337c9e1bd49ac07e53af8fa49899
xander <karimi.xander@gmail.com>
2021-04-21 09:18:32 +0430

    validation sec

--- routes/users.js
+++ routes/users.js
@@ -1,55 +1,9 @@
 const { Router } = require("express");
-const Validator = require("fastest-validator");
-
+const User=require('../models/user');
 const router = new Router();
-const v = new Validator();
+// const v = new Validator();
 
-const schema = {
-    fullname: {
-        type: "string",
-        trim: true,
-        min: 4,
-        max: 255,
-        optional: false,
-        messages: {
-            required: "نام و نام خانوادگی الزامی می باشد",
-            stringMin: "نام و نام خانوادگی نباید کمتر از 4 کاراکتر باشد",
-            stringMax: "نام و نام خانوادگی نباید بیشتر از 255 کاراکتر باشد",
-        },
-    },
-    email: {
-        type: "email",
-        normalize: true,
-        messages: {
-            emailEmpty: "فیلد ایمیل نباید خالی باشد",
-            required: "ایمیل الزامی می باشد",
-            string: "آدرس ایمیل را بررسی کنید",
-        },
-    },
-    password: {
-        type: "string",
-        min: 4,
-        max: 255,
-        messages: {
-            required: "کلمه عبور الزامی می باشد",
-            string: "کلمه عبور را بررسی کنید",
-            stringMin: "کلمه عبور نباید کمتر از 4 کاراکتر باشد",
-            stringMax: "کلمه عبور نمی تواند بیشتر از 255 کاراکتر باشد",
-        },
-    },
-    confirmPassword: {
-        type: "string",
-        min: 4,
-        max: 255,
-        messages: {
-            required: "تکرار کلمه عبور الزامی می باشد",
-            string: "تکرار کلمه عبور را بررسی کنید",
-            stringMin: "تکرار کلمه عبور نباید کمتر از 4 کاراکتر باشد",
-            stringMax: "تکرار کلمه عبور نباید بیشتر از 255 کاراکتر باشد",
-        },
-    },
-    $$strict: true,
-};
+
 
 //  @desc   Login Page
 //  @route  GET /users/login
@@ -68,30 +22,18 @@ router.get("/register", (req, res) => {
 
 //  @desc   Register Handle
 //  @route  POST /users/register
-router.post("/register", (req, res) => {
-    const validate = v.validate(req.body, schema);
-    const errorArr = [];
-    if (validate === true) {
-        const { fullname, email, password, confirmPassword } = req.body;
-        console.log(fullname);
-        console.log(req.body);
-        if (password !== confirmPassword) {
-            errorArr.push({ message: "کلمه های عبور یکسان نیستند" });
-
-            return res.render("register", {
-                pageTitle: "ثبت نام کاربر",
-                path: "/register",
-                errors: errorArr,
-            });
-        }
-        res.redirect("/users/login");
-    } else {
-        console.log(validate);
-        res.render("register", {
-            pageTitle: "ثبت نام کاربر",
-            path: "/register",
-            errors: validate,
-        });
+router.post("/register", async (req, res) => {
+    try {
+        await User.userValidation(req.body)
+        res.redirect("/users/login")
+    } catch (error) {
+        console.log(error)
+        const errors=[]
+        error.inner.forEach(e=>{
+            errors.push(e.path)
+            message=e.message
+        })
+        return res.render('registere',{pageTitle:"ثبت نام",path:"/registere",errors})
     }
 });
 

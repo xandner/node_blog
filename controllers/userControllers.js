c184251e47d36566401c56da3380f9fdb31a89c8
xander <karimi.xander@gmail.com>
2021-04-24 20:03:02 +0430

    hash password

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,27 +1,41 @@
-const User=require('../models/user');
+const User = require('../models/user');
+const bcrypt = require('bcryptjs');
 
-exports.login=(req,res)=>{
+exports.login = (req, res) => {
     res.render("login", { pageTitle: "ورود به بخش مدیریت", path: "/login" });
 }
-exports.register=(req,res)=>{
+exports.register = (req, res) => {
     res.render("registere", {
         pageTitle: "ثبت نام کاربر جدید",
         path: "/registere",
     });
 }
 
-exports.createUser=async(req,res)=>{
+exports.createUser = async (req, res) => {
+    const errors = []
     try {
         await User.userValidation(req.body)
-        await User.create(req.body)
-        res.redirect("/users/login")
+        const { fullname, password, email } = req.body
+        const user = await User.findOne({ email })
+
+        if (user) {
+            errors.push('این ایمیل تکراری است')
+            return res.render('registere', { pageTitle: "ثبت نام", path: "/registere", errors })
+        }
+        bcrypt.genSalt(10, (err, salt) => {
+            if (err) throw err
+            bcrypt.hash(password, salt, async (err, hash) => {
+                if (err) throw err
+                await User.create({ fullname, email, password: hash })
+                res.redirect("/users/login")
+            })
+        })
     } catch (error) {
         console.log(error)
-        const errors=[]
-        error.errors.forEach(e=>{
+        error.errors.forEach(e => {
             errors.push(e)
             // message=e.message
         })
-        return res.render('registere',{pageTitle:"ثبت نام",path:"/registere",errors})
+        return res.render('registere', { pageTitle: "ثبت نام", path: "/registere", errors })
     }
 }

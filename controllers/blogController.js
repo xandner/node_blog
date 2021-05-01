deb4195d52da25ccf955200c79416f8200542cb8
xander <karimi.xander@gmail.com>
2021-05-01 14:04:47 +0430

    show posts

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -1,4 +1,41 @@
-const Blog = require('../models/blog');
-const { formatDate } = require('../utils/jalali');
+const Blog = require("../models/blog");
+const { formatDate } = require("../utils/jalali");
+
+exports.getIndex = async (req, res) => {
+    try {
+        const posts = await Blog.find({ status: "public" }).sort({
+            createdAt: "desc",
+        });
+
+        res.render("index", {
+            pageTitle: "وبلاگ",
+            path: "/",
+            posts,
+            formatDate,
+        });
+    } catch (err) {
+        console.log(err);
+        res.render("errors/500");
+    }
+};
 
 
+exports.getSinglePost = async (req, res) => {
+    try {
+        const post = await Blog.findOne({ _id: req.params.id }).populate(
+            "user"
+        );
+
+        if (!post) return res.redirect("errors/404");
+
+        res.render("post", {
+            pageTitle: post.title,
+            path: "/post",
+            post,
+            formatDate,
+        });
+    } catch (err) {
+        console.log(err);
+        res.render("errors/500");
+    }
+};
--- routes/blog.js
+++ routes/blog.js
@@ -1,14 +1,13 @@
+const controller=require('../controllers/blogController');
+
 const { Router } = require("express");
 
 const router = new Router();
 
 //  @desc   Weblog Index Page
 //  @route  GET /
-router.get("/", (req, res) => {
-    res.render("index", {
-        pageTitle: "وبلاگ",
-        path: "/",
-    });
-});
+router.get("/",controller.getIndex)
+
+router.get("/post/:id", controller.getSinglePost);
 
 module.exports = router;
